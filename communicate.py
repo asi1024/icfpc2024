211fc5c8e91b409deea0e34a2ddeb1091a732f01
Akifumi Imanishi <akifumi.imanishi@gmail.com>
2024-06-28 21:46:26 +0900

    update communicate

--- communicate.py
+++ communicate.py
@@ -1,15 +1,18 @@
 import requests
 import json
-import pandas
-from datetime import datetime, timedelta
+import convert_string as C
 
 headers = {
     'content-type': 'application/json',
     "Authorization": "Bearer ad978f90-614e-47e4-8f75-f013be2cac97",
 }
 
-data = json.dumps({"body": "S'%4}).$%8"})
+text = "Yget index"
+data = C.to_icfp(text)
+print(data)
 resp = requests.post("https://boundvariable.space/communicate", headers=headers, data=data)
 print(resp.text)
+resp = C.from_icfp(resp.text)
+print(resp)
 
 
