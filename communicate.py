c2bce1de9a4457fd9cdd90c595612084d4de6108
Contramundum <contramundum53@outlook.com>
2024-06-29 19:12:16 +0900

    lambdaman09

--- communicate.py
+++ communicate.py
@@ -7,12 +7,15 @@ headers = {
     "Authorization": "Bearer ad978f90-614e-47e4-8f75-f013be2cac97",
 }
 
-text = "solve language_test 4w3s0m3"
-data = C.to_icfp(text)
+data = """B. S3/,6%},!-"$!-!.^} B$ LF B$ vF B. B$ vF SLL B$ vF SFF Lt B$ Lf B. B$ vf B$ vf B$ vf vt S> Ls B. vs B. vs vs"""
+# data = C.to_icfp(text)
+# data = f"B. {C.to_icfp('echo ')} {program}"
 print(">", data)
 resp = requests.post("https://boundvariable.space/communicate", headers=headers, data=data)
 print("<", resp.text)
 resp = C.from_icfp(resp.text)
 print(resp)
+
+
 
 
